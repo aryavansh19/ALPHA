4e99f418c04d9079f7abba222bc51fd6b220a588
Aryavansh Saini <aryavansh.120@gmail.com>
2025-05-06 23:39:18 +0530

    Folder creation Sucessfull

---
+++ commands/command_registry.py
@@ -0,0 +1,7 @@
+from commands.folder.create import create_folder
+from commands.folder.delete import delete_folders
+
+executable_functions = {
+    "create_folder": create_folder,
+    "delete_folders": delete_folders,
+}
